cab220b799af20aeb7d303a5f29a49657ef74fa7
YannyTwoo <79789927+YannyTwoo@users.noreply.github.com>
2022-05-08 23:46:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let calcbtns = document.querySelectorAll('.calcbtns');
+let expression ='';
+Array.from(calcbtns).forEach((btn)=>{
+    //we separated all the buttons with .calcbuts using Array.from() and now applying the code below on the Buttons
+    btn.addEventListener('click', (e)=>{
+        if(e.target.innerHTML == '='){
+            expression = eval(expression);
+            document.querySelector('input').value = expression;
+        }
+        else if(e.target.innerHTML == 'C'){
+            expression = '';
+            document.querySelector('#result').value = expression;
+        }
+        else if(e.target.innerHTML == '%'){
+            expression ='('+ expression + '/100)*';
+            document.querySelector('#result').value = expression;
+        }
+        else{
+            expression += e.target.innerHTML;
+            document.querySelector('#result').value = expression;
+        }
+    })
+})
